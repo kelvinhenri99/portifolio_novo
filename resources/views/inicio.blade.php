cdfd912f2e96b346eb3019942de7583e23ecc676
kelvinhenri99 <kelvin5henri@gmail.com>
2022-06-09 12:48:20 -0300

    Implementação

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -17,7 +17,28 @@
 <div class="container2">
     <div class="div2">
         <h1>MINHAS HABILIDADES</h1>
-        <p>Comecei a escrever os meus primeiros códigos lá em 2013 e desde então nunca parei de estudar e desenvolver soluções através da tecnologia.</p>
+        <p>Comecei a escrever os meus primeiros códigos lá em 2013 e desde então nunca parei de estudar e desenvolver soluções através da tecnologia. Com FRONTEND trabalho hoje com HTML5, CSS3, JavaScript (AJAX/JQUERY/REACT/VUE/TYPESCRIPT), em BACKEND com PHP (LARAVEL), Node.js, MySQL, SQL Server. Com o tempo também adquiri experiências em intefaces do usuário e em design (UX/UI).</p>
+        <a href="">Conheça mais sobre Kelvin Henrique!</a>
+            <div class="imgs">
+            <div class="css3"></div>
+            <div class="html5"></div>
+            <div class="typescript"></div>
+            <div class="react"></div>
+            <div class="node"></div>
+            <div class="javascript"></div>
+            <div class="laravel"></div>
+            <div class="php"></div>
+            <div class="vue"></div>
+            <div class="mysql"></div>
+            <div class="sqlserver"></div>
+            <div class="ux"></div>
+            <div class="ui"></div>
+        </div>
+    </div>
+</div>
+<div class="container3">
+    <div class="div3">
+
     </div>
 </div>
 
--- resources/views/layout/sistema.blade.php
+++ resources/views/layout/sistema.blade.php
@@ -7,7 +7,7 @@
     <title>Kelvin Henrique Ferreira</title>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../css/estilos.css"/>
 </head>
 <body>
     @yield('content')
